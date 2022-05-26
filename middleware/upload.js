07857a438f49ca1f4ed297ee6d158c1e27664f19
korrawee <korrawee.somyos@gmail.com>
2022-05-26 12:13:10 +0700

    Add: multer-s3 to fix file not found

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,26 @@
-const multer = require('multer')
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const bucket_name = 'elasticbeanstalk-ap-southeast-1-331815065301';
 
-const storage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-        callBack(null, './public/uploads/')    
-    },
-    filename: (req, file, callBack) => {
+const s3 = new aws.S3();
+aws.config.update({
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    region: 'ap-southeast-1'
+});
+
+
+const storage = multerS3({
+    s3: s3,
+    acl: 'public-read',
+    bucker: bucket_name,
+    key: (req, file, callBack) => {
         try{
             console.log(file.originalname)
             callBack(null, file.fieldname + req.sessionID + '.csv' )
         }catch{
-            fs.mkdir(path.join(__dirname, './public/uploads/'), (err) => {
+            fs.mkdir(path.join(__dirname, '/public/uploads/'), (err) => {
                 if (err) {
                     return console.error(err);
                 }
